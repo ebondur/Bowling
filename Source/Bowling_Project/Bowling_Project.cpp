627ff56d2bb77404fb26651ac8685be2f694a594
evanbondura <ebondur@clemson.edu>
2022-12-06 15:33:46 -0500

    PLEASE

---
+++ Source/Bowling_Project/Bowling_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Bowling_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Bowling_Project, "Bowling_Project" );
---
+++ Source/Bowling_Project/Bowling_ProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Bowling_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWLING_PROJECT_API ABowling_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+    //this is a test commit
+    //please work
+	
+};
