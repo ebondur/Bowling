dec808d8bd6af015e418d02d8280adb31edd7f3e
Joe <jaross@g.clemson.edu>
2022-12-07 21:12:30 -0500

    Test Push

---
+++ Source/Bowling_Project/BowlingBallBetter.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "BowlingBallBetter.generated.h"
+
+UCLASS()
+class BOWLING_PROJECT_API ABowlingBallBetter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ABowlingBallBetter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
